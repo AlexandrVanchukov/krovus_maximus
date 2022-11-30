286fc3a89382239d291546c6173be2dd796221b1
Alexandr Vanchukov <61061837+AlexandrVanchukov@users.noreply.github.com>
2022-11-30 18:42:52 +0300

    add_update_game

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import ReactDOM from 'react-dom/client';
 
 function App() {
   
+  update_game();
+  function update_game(){
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST","https://sql.lavro.ru/call.php");
+    let fd = new FormData();
+    fd.append("pname","update_game");
+    fd.append("db","284192");
+    fd.append("p1","1803212990");
+    fd.append("p2","22");
+  }
+
   return (
     <div className="App">
 
